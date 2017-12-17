deb32a5c9d948d1049d9b820f9d9846a5cab5213
Brandon Feist <feist.brandon@gmail.com>
2017-12-16 19:26:22 -0800

    Leave artist event processing to server side

--- src/app/artist/artist-details/artist-details.component.ts
+++ src/app/artist/artist-details/artist-details.component.ts
@@ -26,8 +26,6 @@ export class ArtistDetailsComponent implements OnInit {
     this.paramSubscription = this.activatedRoute.params.subscribe(params => this.artistSlug = params['slug']);
 
     this.artist = this.activatedRoute.snapshot.data['artist'];
-
-    this.processArtistEvents(this.artist.events);
   }
 
   ngOnDestroy() {
@@ -38,31 +36,6 @@ export class ArtistDetailsComponent implements OnInit {
     this.dialog.open(ArtistSpotifyDialog, {
       data: { artist: this.artist }
     });
-  }
-
-  processArtistEvents(events: Event[]) {
-    let indexToSplice: number[] = new Array();
-
-    let now = new Date();
-    now.setHours(23);
-    now.setMinutes(59);
-    now.setSeconds(59);
-    now.setMilliseconds(999);
-
-    for(let eventIndex = (events.length - 1); eventIndex >= 0; eventIndex--) {
-
-      let eventStartDate = new Date(events[eventIndex].startDateTime);
-      eventStartDate.setHours(23);
-      eventStartDate.setMinutes(59);
-      eventStartDate.setSeconds(59);
-      eventStartDate.setMilliseconds(999);
-      
-      console.log("check: " + (eventStartDate.getDay() - now.getDay()));
-      
-      if((eventStartDate.getDay() - now.getDay()) < 0) {
-        events.splice(eventIndex, 1);
-      }
-    }
   }
 
   formatReleaseArtists(release): string {
